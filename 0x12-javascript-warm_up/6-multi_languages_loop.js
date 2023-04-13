7ea5ab6df43b9c440b89086b6e6981ce7ea17d59
Chinonso Onaga <76971567+chinonsoScelloo@users.noreply.github.com>
2023-04-14 00:11:11 +0100

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const languages = ["C", "Python", "JavaScript"];
+const adjectives = ["fun", "cool", "amazing"];
+
+for (const [index, language] of languages.entries()) {
+  console.log(`${language} is ${adjectives[index]}`);
 }
+
