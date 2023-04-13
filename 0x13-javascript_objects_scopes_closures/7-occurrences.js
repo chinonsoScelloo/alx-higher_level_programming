8d65d051c474920bfa424eabb70eabcd3429178a
Chinonso Onaga <76971567+chinonsoScelloo@users.noreply.github.com>
2023-04-14 00:15:13 +0100

    update

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,6 @@
+const languages = ["C", "Python", "JavaScript"];
+const adjectives = ["fun", "cool", "amazing"];
+
+for (const [index, language] of languages.entries()) {
+  console.log(`${language} is ${adjectives[index]}`);
+}
